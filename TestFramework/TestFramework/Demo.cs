99b1e2cd7dcd6c4bc612e4e6619d73beadcf5184
bluesbox1 <40844619+bluesbox1@users.noreply.github.com>
2024-06-20 19:06:47 +0800

    Add files via upload

---
+++ TestFramework/TestFramework/Demo.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestFramework
+{
+    internal class Demo
+    {
+        IWebDriver driver;
+
+        [SetUp]
+        public void startBrowser()
+        {
+            driver = new ChromeDriver(Path.GetFullPath("C:\\Users\\User\\source\\repos\\TestFramework\\TestFramework\\drivers"));
+        }
+
+        [Test]
+        public void test()
+        {
+            driver.Url = "http://www.google.co.in";
+        }
+
+        [TearDown]
+        public void closeBrowser()
+        {
+            driver.Close();
+        }
+    }
+}
