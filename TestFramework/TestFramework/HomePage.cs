b9eb180ddb10f3481e32f192be0bb5006e5b050f
bluesbox1 <40844619+bluesbox1@users.noreply.github.com>
2024-06-21 12:17:52 +1200

    add page object and webaction wrapper classes

---
+++ TestFramework/TestFramework/HomePage.cs
@@ -0,0 +1,36 @@
+﻿using OpenQA.Selenium;
+
+namespace TestFramework.PageObject
+{
+    public class HomePage
+    {
+        private readonly WebActions _webActions;
+        private readonly By _searchBox = By.Name("q");
+        private readonly By _searchButton = By.Name("btnK");
+
+        public HomePage(WebActions webActions)
+        {
+            _webActions = webActions;
+        }
+
+        public void OpenHomePage()
+        {
+            _webActions.NavigateToUrl("https://www.google.com");
+        }
+
+        public void EnterSearchText(string searchText)
+        {
+            _webActions.EnterText(_searchBox, searchText);
+        }
+
+        public void ClickSearchButton()
+        {
+            _webActions.ClickElement(_searchButton);
+        }
+
+        public bool IsSearchBoxDisplayed()
+        {
+            return _webActions.IsElementDisplayed(_searchBox);
+        }
+    }
+}
---
+++ TestFramework/TestFramework/WebActions.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace TestFramework
+{
+    public class WebActions
+    {
+        private readonly IWebDriver _driver;
+
+        public WebActions(IWebDriver driver)
+        {
+            _driver = driver;
+        }
+
+        public void NavigateToUrl(string url)
+        {
+            _driver.Navigate().GoToUrl(url);
+        }
+
+        public void ClickElement(By locator)
+        {
+            _driver.FindElement(locator).Click();
+        }
+
+        public void EnterText(By locator, string text)
+        {
+            _driver.FindElement(locator).SendKeys(text);
+        }
+
+        public string GetElementText(By locator)
+        {
+            return _driver.FindElement(locator).Text;
+        }
+
+        public bool IsElementDisplayed(By locator)
+        {
+            try
+            {
+                return _driver.FindElement(locator).Displayed;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+        }
+    }
+}
